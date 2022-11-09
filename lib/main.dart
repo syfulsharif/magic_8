9fc0453ce1da64a04d2d584841c0e12fdfd427cc
sharif.syful@gmail.com <sharif.syful@gmail.com>
2022-11-10 00:52:59 +0600

    Centered Image Adde

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,12 @@ class _BallState extends State<Ball> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset('images/ball1.png'),
+      child: TextButton(
+        onPressed: () {
+          print('Button Pressed');
+        },
+        child: Image.asset('images/ball1.png'),
+      ),
     );
   }
 }
